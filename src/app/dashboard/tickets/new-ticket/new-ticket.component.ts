90bd4e5acd17aef53610e2e2aa3c786430a77e4e
Sayem Prodhan <ananta.ap69@gmail.com>
2024-07-15 11:57:24 +0600

    finished

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,7 +1,8 @@
-import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, model, Output, output, viewChild, ViewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from "../../../shared/control/control.component";
 import { FormsModule } from '@angular/forms';
+import { TicketModel } from '../ticket.model';
 
 @Component({
   selector: 'app-new-ticket',
@@ -13,10 +14,22 @@ import { FormsModule } from '@angular/forms';
 export class NewTicketComponent {
   // @ViewChild('form') form?: ElementRef<HTMLFormElement>;
   private form = viewChild<ElementRef<HTMLFormElement>>('form');
+  add = output<{
+    title: string,
+    text: string
+  }>()
+
+  //2 way binding!!
+  // @Input() varName: string = ''
+  // @Output() varNameChange = new EventEmitter<string>()
+  //2 way binding >=17
+  // varName = model.required<string>()
+
   // onSubmit(title: string, requestText: string, form: HTMLFormElement) {
   //   form.reset();
   // }
   onSubmit(title: string, requestText: string) {
+    this.add.emit({ title: title, text: requestText })
     this.form()?.nativeElement.reset();
   }
 
--- src/app/dashboard/tickets/tickets.component.ts
+++ src/app/dashboard/tickets/tickets.component.ts
@@ -1,13 +1,36 @@
 import { Component } from '@angular/core';
 import { NewTicketComponent } from "./new-ticket/new-ticket.component";
+import { TicketModel } from './ticket.model';
+import { Title } from '@angular/platform-browser';
+import { TicketComponent } from "./ticket/ticket.component";
 
 @Component({
   selector: 'app-tickets',
   standalone: true,
-  imports: [NewTicketComponent],
+  imports: [NewTicketComponent, TicketComponent],
   templateUrl: './tickets.component.html',
   styleUrl: './tickets.component.css'
 })
 export class TicketsComponent {
+  tickets: TicketModel[] = []
 
+  onAdd(event: { title: string; text: string; }) {
+    const ticket: TicketModel = {
+      id: Math.random().toString(),
+      status: 'open',
+      title: event.title,
+      request: event.text
+    }
+    this.tickets.unshift(ticket)
+  }
+
+
+  onCloseTicket(id: string) {
+    this.tickets = this.tickets.map((ticket) => {
+      if (ticket.id === id) {
+        return { ...ticket, status: 'closed' }
+      }
+      return ticket
+    })
+  }
 }
